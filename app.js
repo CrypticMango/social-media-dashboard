a743083f6e5d7d2ed894a7aafdc0d3ef1840040a
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-01-01 12:07:52 -0600

    grid styling for desktop
    -removed comments
    -clear class name on toggle in js

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-const input = document.querySelector("input");
-const slider = document.getElementById("slider");
-var bgColor = slider.getAttributeNames("background-color");
-
 const toggle = document.getElementById("toggle-switch");
 const body = document.body;
 
-//click body and theme switches
-
-body.addEventListener("click", toggleLight);
-
-function toggleLight() {
-	body.classList.toggle("light-mode");
-}
-
-//use toggle switch
-
 toggle.addEventListener("input", (e) => {
+   
 	const isChecked = e.target.checked;
 
 	if (isChecked) {
 		body.classList.add("light-mode");
+        body.classList.remove("dark-mode");
 	} else {
 		body.classList.add("dark-mode");
+        body.classList.remove("light-mode");
 	}
 });
-
-//logic for toggle function. if toggle background is green-blue then gradient theme is dark. if toggle background is grey then theme is light.
-
-//figure out how to get background color value
