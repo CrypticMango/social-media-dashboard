452902201996fe84e8eb2e3c38b528f750e47291
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-01-01 11:29:36 -0600

    updated code for theme switcher

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
-const body = document.body;
-const toggle = document.getElementById("toggle-switch");
 const input = document.querySelector("input");
 const slider = document.getElementById("slider");
 var bgColor = slider.getAttributeNames("background-color");
 
-console.log(toggle.toggleAttribute(true))
-console.log(toggle);
-console.log(input.checked);
-console.log(toggle.style.backgroundColor);
-console.log(slider.style.background);
-console.log(body.style.backgroundColor);
+const toggle = document.getElementById("toggle-switch");
+const body = document.body;
+
+//click body and switches theme
 
 body.addEventListener("click", toggleLight);
 
 function toggleLight() {
-    body.classList.toggle("light-mode");
+	body.classList.toggle("light-mode");
 }
+
+//use toggle switch and theme switches
+
+toggle.addEventListener("input", (e) => {
+	const isChecked = e.target.checked;
+
+	if (isChecked) {
+		body.classList.add("light-mode");
+	} else {
+		body.classList.add("dark-mode");
+	}
+});
 
 //logic for toggle function. if toggle background is green-blue then gradient theme is dark. if toggle background is grey then theme is light.
 
